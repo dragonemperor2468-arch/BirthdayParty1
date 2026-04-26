c5e4ce0a6dfc0be9b41b437a3e0c45b6dbfe10f3
dragonemperor2468-arch <dragonemperor2468@gmail.com>
2026-04-26 21:09:06 +0530

    Final Party Code

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLocation() {
+    // Ye raha tera link
+    const locationLink = "https://www.google.com/search?q=Firangish+Cafe+%26+Courtyard";
+    window.open(locationLink, "_blank");
+}
+
+console.log("System Initialized: The Unholy Trinity Bash is Live!");
